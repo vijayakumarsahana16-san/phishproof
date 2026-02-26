cc6d9ee6aea65bcf3867720330ebd5961b01fbd4
Sahana Vijayakumar <vijayakumarsahana16@gmail.com>
2026-02-26 10:03:32 +0530

    Third commit

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
     try {
       // 3. Make the API Call to FastAPI (running on port 8000)
-      const response = await fetch('http://localhost:8000/analyze', {
+      const response = await fetch('https://phishproof.onrender.com/analyze', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
